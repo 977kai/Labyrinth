c6b415d89bfd6fb92d74f46026d33da5823ebc6b
alex <karuk1998@yandex.ru>
2020-07-15 10:14:18 +0300

    small changes

--- labyrinth.py
+++ labyrinth.py
@@ -2,7 +2,7 @@ class Labyrinth:
     """This class provides labyrinth object"""
 
     def __init__(self):
-        self._labyrinth = [
+        self.__labyrinth = [
             ['-', '-', '-', '-', '-', '-', '-'],
             ['|', ' ', ' ', ' ', '|', ' ', 'X'],
             ['|', ' ', '|', ' ', '|', ' ', '|'],
@@ -10,6 +10,9 @@ class Labyrinth:
             ['|', ' ', '|', ' ', ' ', ' ', '|'],
             ['-', '-', '-', '-', '-', '-', '-']
         ]
+        self.__borders = ['-', '|']
+
+    borders = property(lambda self: self.__borders)
 
     def __str__(self):
         """
@@ -17,16 +20,27 @@ class Labyrinth:
             (str): String representation of labyrinth current state
         """
         labyrinth_str = ''
-        for level in self._labyrinth:
+        for level in self.__labyrinth:
             labyrinth_str += ''.join(level) + '\n'
         return labyrinth_str
+
+    def __getitem__(self, coordinates):
+        """
+        Return value of labyrinth cell by coords
+
+        Args:
+            coordinates (utils.Coordinates): Coordinates of labyrinth cell
+        Returns:
+            (str): Value of labyrinth cell
+        """
+        return self.__labyrinth[coordinates.y][coordinates.x]
 
     def update_cell(self, coordinates, value):
         """
         Update labyrinth cell with given coordinates by new value
 
         Args:
-            coordinates (tuple of int): (x, y) coordinates of labyrinth cell
+            coordinates (utils.Coordinates): Coordinates of labyrinth cell
             value (str): New value to be inserted into cell
         """
-        self._labyrinth[coordinates[1]][coordinates[0]] = value
+        self.__labyrinth[coordinates.y][coordinates.x] = value
